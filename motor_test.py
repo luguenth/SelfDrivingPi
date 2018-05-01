53afbba109aa0d22bb881b320789a77ea1397c9c
lellebebbl <lukas.guenther94@web.de>
2018-05-01 20:20:05 +0000

    refactored motor code

--- motor_test.py
+++ motor_test.py
@@ -6,12 +6,17 @@ import time
 PIN_PWM1 = 12
 PIN_PWM2 = 18
 
+PIN_MOTOR1P = 23
+PIN_MOTOR1N = 24
+PIN_MOTOR2P = 17
+PIN_MOTOR2N = 22
+
 gpio.setmode(gpio.BCM)
 
-gpio.setup(17, gpio.OUT)
-gpio.setup(22, gpio.OUT)
-gpio.setup(23, gpio.OUT)
-gpio.setup(24, gpio.OUT)
+gpio.setup(PIN_MOTOR2P, gpio.OUT)
+gpio.setup(PIN_MOTOR2N, gpio.OUT)
+gpio.setup(PIN_MOTOR1P, gpio.OUT)
+gpio.setup(PIN_MOTOR1N, gpio.OUT)
 
 # pwm pins
 gpio.setup(PIN_PWM2, gpio.OUT) # right motor
@@ -23,53 +28,63 @@ pwms = [pwml, pwmr]
 pwml.start(0)
 pwmr.start(0)
 
-def stop():
+def stop(x = True,y = True):
     for pwm in pwms:
         pwm.ChangeDutyCycle(0)
     #gpio.output(PIN_PWM1, False)
     #gpio.output(PIN_PWM2, False)
 
 
-def init(): 
-    for pwm in pwms:
-        pwm.ChangeDutyCycle(20)
-    #gpio.output(PIN_PWM1, True)
-    #gpio.output(PIN_PWM2, True)
+def init(x = True,y = True): 
+    dc = 70
+    dc2 = 30
+    if x:
+        pwml.ChangeDutyCycle(dc)
+    if y: 
+        pwmr.ChangeDutyCycle(dc)
+
+
+
+def motor1_rev():
+    gpio.output(PIN_MOTOR1P, True)
+    gpio.output(PIN_MOTOR1N, False)
+
+def motor1_for():
+    gpio.output(PIN_MOTOR1P, False)
+    gpio.output(PIN_MOTOR1N, True)
+
+def motor2_rev():
+    gpio.output(PIN_MOTOR2P, True)
+    gpio.output(PIN_MOTOR2N, False)
+
+def motor2_for():
+    gpio.output(PIN_MOTOR2P, False)
+    gpio.output(PIN_MOTOR2N, True)
 
 
 def reverse(tf):
  init()
- gpio.output(17, True)
- gpio.output(22, False)
- gpio.output(23, True) 
- gpio.output(24, False)
+ motor1_rev()
+ motor2_rev()
  time.sleep(tf)
  stop()
 
 def forward(tf):
  init()
- gpio.output(17, False)
- gpio.output(22, True)
- gpio.output(23, False) 
- gpio.output(24, True)
+ motor1_for()
+ motor2_for()
  time.sleep(tf)
  stop()
 
 def left(tf):
- init()
- gpio.output(17, 0)
- gpio.output(22, 1)
- gpio.output(23, 1)
- gpio.output(24, 1)
+ init(True, False)
+ motor1_for()
  time.sleep(tf)
  stop()
 
 def right(tf):
- init()
- gpio.output(17, 1)
- gpio.output(22, 1)
- gpio.output(23, 0) 
- gpio.output(24, 1)
+ init(False, True)
+ motor2_for()
  time.sleep(tf)
  stop()
 
